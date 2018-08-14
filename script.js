8ac314be432b8c685bc3c636c4e8ee21659fce14
Shai <shaistakirmani@gmail.com>
2018-08-14 16:20:50 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const userFoodChoice = {
+    spicy:[
+        {
+            name: 'Khao San Road',
+            price:'$$$'
+        },
+        {
+            name:'Papa Spicy',
+            price:'$$'
+        },
+        {
+            name:'Saffron spice Kitchen',
+            price:'$'
+        }
+    ],
+    savory:[
+        {
+            name:'Planta',
+            price:'$$$'
+        },
+        {
+            name:'Hogtown Vegan',
+            price:'$$'
+        },
+        {
+            name:'Greens Vegetarian',
+            price:'$'
+        }
+    ],
+    sweet:[
+        {
+            name: 'Bunners',
+            price:'$$$'
+        },
+        {
+            name:'Sweet Oklenkas',
+            price:'$$'
+        },
+        {
+            name:'Through Being Cool',
+            price:'$'
+
+        }
+    ]
+}
